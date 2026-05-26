c378c408a119e39fc6ba6e727c76c54671f80d76
Tim Aksu <timbo@timbojimbo.com>
2026-05-26 21:37:00 +1000

    Fixed Null Ref in TMPTextLocalizer

--- Packages/com.timbojimbo.localization/CHANGELOG.md
+++ Packages/com.timbojimbo.localization/CHANGELOG.md
@@ -1,4 +1,8 @@
-## [1.1.0] - 2026-05-24
+## [1.2.0] - 2026-05-26
+
+- Fixed Null Refs in TMPTextLocalizer
+
+## [1.1.0] - 2026-05-26
 
 - Submitted package to OpenUPM.
 - Updated installation instructions. 
--- Packages/com.timbojimbo.localization/Runtime/Applicators/TMPTextLocalizer.cs
+++ Packages/com.timbojimbo.localization/Runtime/Applicators/TMPTextLocalizer.cs
@@ -1,11 +1,26 @@
 using System;
 using TMPro;
+using UnityEngine;
 
 namespace TimboJimbo.Localization.Appicators
 {
     public class TMPTextLocalizer : TextLocalizer, ITextPreprocessor
     {
-        public TextMeshProUGUI Target;
+        public TextMeshProUGUI Target
+        {
+            get => _target;
+            set
+            {
+                if (_target == value) return;
+
+                Unbind();
+                _target = value;
+                Bind();
+            }
+        }
+
+        [SerializeField] private TextMeshProUGUI _target;
+        [NonSerialized] private TextMeshProUGUI _boundTarget;
         [NonSerialized] private string _lastAppliedText;
 
         public string PreprocessText(string text)
@@ -16,30 +31,76 @@ namespace TimboJimbo.Localization.Appicators
         protected override void OnEnable()
         {
             base.OnEnable();
-            if (Target != null) Target.textPreprocessor = this;
+            Bind();
         }
 
         protected override void OnDisable()
         {
             base.OnDisable();
-
-            if (Target != null && Target.textPreprocessor == (ITextPreprocessor)this)
-                Target.textPreprocessor = null;
+            Unbind();
 
         }
 
         protected override void OnValidate()
         {
             base.OnValidate();
-            if (Target == null && enabled) Target.textPreprocessor = this;
+
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.delayCall += () =>
+            {
+                if (this == null || _target == _boundTarget) return;
+                Unbind();
+                Bind();
+            };
+            #endif
         }
 
         protected override void ApplyTextToTarget(string text)
         {
             _lastAppliedText = text;
 
-            if (Target != null)
-                Target.SetAllDirty();
+            if (_boundTarget != null)
+                _boundTarget.SetAllDirty();
+        }
+
+        private void Unbind()
+        {
+            if (_boundTarget == null) return;
+
+            if (_boundTarget.textPreprocessor == (ITextPreprocessor)this)
+                _boundTarget.textPreprocessor = null;
+            
+            RefreshTarget();
+            _boundTarget = null;
+        }
+
+        private void Bind()
+        {
+            if (_target == null) return;
+
+            _boundTarget = _target;
+
+            /// We apply text via ITextPreprocessor to avoid stomping over any existing content in the TMP text field.
+            _boundTarget.textPreprocessor = this;
+            RefreshTarget();
+        }
+
+        private void RefreshTarget()
+        {
+            if (_boundTarget == null) return;
+
+            _boundTarget.SetAllDirty();
+            
+            #if UNITY_EDITOR
+            // Only way I could figure out how to get TMP to update the text in edit mode
+            // SetAllDirty doesn't seem to be enough, since I guess the text hasn't actually
+            // 'changed' - only the ITextProcessor has..? Not sure...!
+            if(!Application.isPlaying)
+            {
+                _boundTarget.enabled = false;
+                _boundTarget.enabled = true;
+            }
+            #endif
         }
     }
 }
